6d22ec6a7694c13903186b6f79bd7d91a7f4087e
leonilso <wrublak.leonilso@escola.pr.gov.br>
2025-12-18 09:50:16 -0300

    build

--- src/pages/Experiencia.jsx
+++ src/pages/Experiencia.jsx
@@ -1,15 +1,46 @@
+import ExperienciaCard from "../components/ExperienciaCard"
+
 function Experiencia() {
+
+  const minhasExperiencias = [
+    {
+      profissao: "Professor de programação",
+      anos: "2022 - 2025",
+      descricao: "Aulas de programação, gestão de processos, variádos temas"
+    },
+    {
+      profissao: "Secretário de polo de Faculdade",
+      anos: "2021 - 2025",
+      descricao: "Automação de processos, envio de documentos, atendimento ao público"
+    },
+    {
+      profissao: "Professor Formador",
+      anos: "2023 - 2025",
+      descricao: "Ministrando cursos na área de tecnologia Scratch, IA, planilhas, além de automações com Classroom, Google Sheets WhatsApp e Python"
+    },
+    {
+      profissao: "Atendente de mercado",
+      anos: "2019-2021",
+      descricao: "Atendimento ao público, envio de notas e configuração de sistema"
+    },
+
+
+  ]
+
   return (
     <div className="fade-in">
       <h1 style={{color: 'var(--primary-green)'}}>Experiência & Cursos</h1>
       
       <section>
-        <h2>Experiência Profissional</h2>
-        <div className="card">
-          <h3>Desenvolvedor Front-end - Empresa X</h3>
-          <p>2022 - Presente</p>
-          <p>Trabalhando com React, Vite e integração de APIs.</p>
-        </div>
+        {minhasExperiencias.map((experiencia, index) => (
+          <ExperienciaCard
+            key ={index}
+            profissao = {experiencia.profissao}
+            anos = {experiencia.anos}
+            descricao = {experiencia.descricao}
+          />
+        ))}
+
       </section>
 
       <section style={{marginTop: '2rem'}}>
